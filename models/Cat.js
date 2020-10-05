6c508f5cd18b009d00d13fa7a024533b55fc793b
Howakyl96 <howakyl@gmail.com>
2020-10-05 14:43:40 -0700

    commit 4 - swapped cat array with catSchema

--- models/Cat.js
+++ models/Cat.js
@@ -1,21 +1,51 @@
-const cats = [{
-    name: 'Fuzzy',
-    breed: 'russian blue',
-    color: 'grey',
-    likes: ['playing with yarn', 'biting'],
-    longHaired: false,
-}, {
-    name: 'Fluffy',
-    breed: 'calico',
-    color: 'calico',
-    likes: ['sleeping' , 'scratching couches'],
-    longHaired: false,
-}, {
-    name: 'Pirate',
-    breed: 'bombay',
-    color: 'black',
-    likes: ['chasing mice' , 'climbing up to high places'],
-    longHaired: false,
-}];
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CatSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 50,
+    },
+    breed: {
+        type: String,
+    },
+    color: {
+        type: String,
+    },
+    likes: [
+        {
+            type: String,
+        }
+    ],
+    longHaired: {
+        type: Boolean,
+    }
+} , {timestamps: true});
+
+module.exports = mongoose.model('Fruit' , CatSchema);
+
+
+
+// const cats = [{
+//     name: 'Fuzzy',
+//     breed: 'russian blue',
+//     color: 'grey',
+//     likes: ['playing with yarn', 'biting'],
+//     longHaired: false,
+// }, {
+//     name: 'Fluffy',
+//     breed: 'calico',
+//     color: 'calico',
+//     likes: ['sleeping' , 'scratching couches'],
+//     longHaired: false,
+// }, {
+//     name: 'Pirate',
+//     breed: 'bombay',
+//     color: 'black',
+//     likes: ['chasing mice' , 'climbing up to high places'],
+//     longHaired: false,
+// }];
 
 module.exports = cats;
