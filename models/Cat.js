b64d57d4a13b4635be41510dcfc456313708a201
Howakyl96 <howakyl@gmail.com>
2020-10-05 14:55:40 -0700

    commit 6 - added mongoose config in index.js

--- models/Cat.js
+++ models/Cat.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CatSchema = new Schema ({
+const catSchema = new Schema ({
     name: {
         type: String,
         required: true,
@@ -24,7 +24,7 @@ const CatSchema = new Schema ({
     }
 } , {timestamps: true});
 
-module.exports = mongoose.model('Fruit' , CatSchema);
+module.exports = mongoose.model('Cat' , catSchema);
 
 
 
---
+++ models/index.js
@@ -0,0 +1,18 @@
+//database config
+
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost:27017/express-cats';
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected' , () => console.log('MongoDB connected successfully.'));
+mongoose.connection.on('error' , (err) => console.log(err));
+
+module.exports = {
+    Cat: require('./Cat')
+}
